62146ed4cc821f12414a76c408f2b1b5febb0f10
Kyoto430 <nateriver345@gmail.com>
2024-09-10 16:03:24 +0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,7 @@ function burger() {
 
   burgerBtn.addEventListener('click', function () {
     document.body.classList.toggle('lock');
+    document.body.classList.toggle('mobile-menu-active');
     burgerBtn.classList.toggle('active');
     menu.classList.toggle('active');
     overlay.classList.toggle('active');
@@ -16,6 +17,7 @@ function burger() {
     link.addEventListener('click', function () {
       if (!link.classList.contains('drop__icon')) {
         document.body.classList.remove('lock');
+        document.body.classList.remove('mobile-menu-active');
         burgerBtn.classList.remove('active');
         menu.classList.remove('active');
         overlay.classList.remove('active');
@@ -26,6 +28,7 @@ function burger() {
   document.addEventListener('click', function (event) {
     if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
       document.body.classList.remove('lock');
+      document.body.classList.remove('mobile-menu-active');
       burgerBtn.classList.remove('active');
       menu.classList.remove('active');
       overlay.classList.remove('active');
@@ -147,7 +150,7 @@ function openMenu() {
   });
 }
 
-if (window.innerWidth < 1280) {
+if (window.innerWidth <= 1280) {
   openMenu();
 }
 
@@ -198,3 +201,47 @@ function showMoreText() {
 }
 
 showMoreText();
+
+// Mask phone
+
+window.addEventListener('DOMContentLoaded', function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = '+7 (___) ___ __-__',
+        i = 0,
+        def = matrix.replace(/\D/g, ''),
+        val = this.value.replace(/\D/g, ''),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+      i = new_value.indexOf('_');
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i);
+      }
+      var reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return '\\d{1,' + a.length + '}';
+        })
+        .replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (
+        !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+      )
+        this.value = new_value;
+      if (event.type == 'blur' && this.value.length < 5) this.value = '';
+    }
+
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
+    input.addEventListener('keydown', mask, false);
+  });
+});
